7ac2fc60cc11b492233c6d42d20b7caff24e5830
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-24 12:33:07 +0200

    working on init

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+require("./config/mongodb");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.routes');
+var usersRouter = require('./routes/users.routes');
 
 var app = express();
 
