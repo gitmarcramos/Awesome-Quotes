135e0af06292b4920096dcd4546871c6e6f8fe5e
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-24 12:29:33 +0200

    working on init

---
+++ config/mongodb.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(`Connected to Mongo (DB file)! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
