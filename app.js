e6fbe0202b4cea89b440d1b49540474be3c71cf1
priveron <paul.riveron@outlook.com>
2021-10-25 16:56:14 +0200

    [ADD] ading cookies to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./config/mongodb");
 const createError = require("http-errors");
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -29,6 +30,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
+
+// SESSION SETUP
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
