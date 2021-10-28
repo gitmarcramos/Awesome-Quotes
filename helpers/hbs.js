1e6e4fb1bc898eaa3fe4ea55a827508bd3403f62
priveron <paul.riveron@outlook.com>
2021-10-28 18:42:01 +0200

    [ADD] like (miss favorite but same) and helpers and littles fixs

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -2,10 +2,8 @@ const hbs = require("hbs");
 
 // CUSTOM HELPERS
 hbs.registerHelper("isInArray", function (needle, haystack, options) {
-  console.log('AAA');
   if (haystack !== undefined) {
     const isInArray = haystack.some((id) => {
-      console.log(id, needle);
       return needle.equals(id);
     })
     if (isInArray)
--- middlewares/protectUserRoute.js
+++ middlewares/protectUserRoute.js
@@ -1,4 +1,4 @@
 module.exports = function protectAuthRoute(req, res, next) {
   if (req.session.currentUser) next();
-  else res.redirect("/login");
+  else res.redirect("/auth/login");
 }
